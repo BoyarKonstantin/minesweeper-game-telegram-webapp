ba1ffd125987570895093afa5a4ddca17a8f6159
zhotheone <98022845+zhotheone@users.noreply.github.com>
2023-06-12 23:15:39 +0300

    Delete buttons.py

--- buttons.py
+++
@@ -1,5 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-game = InlineKeyboardMarkup(row_width=1)
-game_button = InlineKeyboardButton(text="Play game!", url='www.google.com')
-game.insert(game_button)
